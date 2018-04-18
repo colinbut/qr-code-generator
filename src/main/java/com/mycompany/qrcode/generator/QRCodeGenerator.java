a229e269c126682316c252f71fbaf9dc055b6c6e
Colin But <colinbut@users.noreply.github.com>
2018-04-18 18:01:02 +0100

    making it work first

---
+++ src/main/java/com/mycompany/qrcode/generator/QRCodeGenerator.java
@@ -0,0 +1,36 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.qrcode.generator;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class QRCodeGenerator {
+
+    public static void main(String[] args) {
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = qrCodeWriter.encode("This is a message hidden in QR Code", BarcodeFormat.QR_CODE, 350, 350);
+
+            Path path = FileSystems.getDefault().getPath("./QRCode.png");
+
+            MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
